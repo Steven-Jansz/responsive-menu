95442d6b9cdea20cfc9ddc18aaacda7916f6686e
Steven-Jansz <Steven-Jansz@users.noreply.github.com>
2015-01-03 21:32:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
 <!DOCTYPE html>
+
 <head>
 <meta http-equiv="CACHE-CONTROL" content="NO-CACHE" />
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-<title>Responsive Menu</title>
+<title>Simple Responsive Menu</title>
 <link rel="stylesheet" type="text/css" href="main.css" />
+<link rel="stylesheet" type="text/css" href="navigation.css" />
 </head>
 <body>
 <div class="page-container">
   <div class="row">
-   <div class="nav-bar clearfix">
-   <a href="#" id="nav-bar-icon">Menu</a>
-        <ul class="clearfix">
-            <li><a href="#" class="transition" >Navi 1</a></li>
-            <li><a href="#" class="transition">Navi 2</a></li>
-            <li><a href="#" class="transition">Navi 3</a></li>
-            <li><a href="#" class="transition">Navi 4</a></li>
-            <li><a href="#" class="transition">Navi 5</a></li>
-            <li><a href="#" class="transition">Navi 6</a></li>	
-        </ul>
-   </nav>   
+    <h1>Simple Responsive Menu</h1>
+    <div class="nav-bar clearfix"> <a href="#" id="nav-bar-icon"><i class="menu"></i></a>
+      <ul class="clearfix">
+        <li><a href="#" class="transition" >Home</a></li>
+        <li><a href="#" class="transition">What we do </a></li>
+        <li><a href="#" class="transition">Portfolio</a></li>
+        <li><a href="#" class="transition">Contact us</a></li>
+      </ul>
+      </nav>
+    </div>
   </div>
 </div>
 <script language="JavaScript" type="text/javascript" src="jquery.js"></script> 
